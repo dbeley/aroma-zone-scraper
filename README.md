32255658e3292797b0bb330265e2374099e389c5
dbeley <6568955+dbeley@users.noreply.github.com>
2019-05-24 19:59:55 +0200

    black code formatting, update README.md

--- README.md
+++ README.md
@@ -2,7 +2,33 @@
 
 Scraper for aroma-zone.com
 
+## Requirements
+
+- requests
+- pandas
+- bs4
+
+## Installation of the virtualenv (recommended)
+
+```
+pipenv install
+```
+
 ## Usage
 ```
 python aroma-zone-scraper.py
+```
+
+```
+python aroma-zone-scraper.py -h
+```
+
+```
+usage: aroma-zone-scraper.py [-h] [--debug]
+
+Scraper aroma-zone.
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --debug     Display debugging information
 ```
--- aroma-zone-scraper.py
+++ aroma-zone-scraper.py
@@ -14,41 +14,49 @@ temps_debut = time.time()
 
 
 def get_soup(url):
-    return BeautifulSoup(requests.get(url).content, features='lxml')
+    return BeautifulSoup(requests.get(url).content, features="lxml")
 
 
 def get_categories(url):
     soup_index = get_soup(url)
     urls_cat = []
 
-    links_index = soup_index.find_all('div', {'class': 'category-block'})
+    links_index = soup_index.find_all("div", {"class": "category-block"})
     for cat in links_index:
-        urls_cat.append(str(cat.find('a')['href']))
+        urls_cat.append(str(cat.find("a")["href"]))
     return urls_cat
 
 
 def get_products(url):
     soup_cat = get_soup(url)
     urls_products = []
-    products = soup_cat.find_all('a', {'class': 'product-link'})
+    products = soup_cat.find_all("a", {"class": "product-link"})
     if not products:
         return urls_products
     for product in products:
-        urls_products.append(product['href'])
+        urls_products.append(product["href"])
     return urls_products
 
 
 def get_specs(url):
     product = {}
     soup_product = get_soup(url)
-    product['Nom'] = str(soup_product.find('div', {'class': 'title-box'}).find('h1').text.strip())
-    product['Lien'] = url
-    product['Prix'] = ''.join(soup_product.find('span', {'class': 'price'}).find(text=True).split())
-    product['Description'] = str(soup_product.find('div', {'class': 'fiche-desc'}).text.strip())
-    identity_card = soup_product.find('div', {'class': 'block-id-card'})
+    product["Nom"] = str(
+        soup_product.find("div", {"class": "title-box"})
+        .find("h1")
+        .text.strip()
+    )
+    product["Lien"] = url
+    product["Prix"] = "".join(
+        soup_product.find("span", {"class": "price"}).find(text=True).split()
+    )
+    product["Description"] = str(
+        soup_product.find("div", {"class": "fiche-desc"}).text.strip()
+    )
+    identity_card = soup_product.find("div", {"class": "block-id-card"})
     try:
-        intitules = identity_card.find_all('div', {'class': 'intitule'})
-        valeurs = identity_card.find_all('div', {'class': 'value'})
+        intitules = identity_card.find_all("div", {"class": "intitule"})
+        valeurs = identity_card.find_all("div", {"class": "value"})
         for i, v in zip(intitules, valeurs):
             intitule = str(i.text.replace(":", "").rstrip())
             valeur = str(v.text.strip())
@@ -89,7 +97,7 @@ def main():
         logger.debug("Creating Exports Folder")
         os.makedirs(directory)
 
-    df = pd.DataFrame.from_dict(products_dict, orient='index')
+    df = pd.DataFrame.from_dict(products_dict, orient="index")
     try:
         filename = f"{directory}/aromazone_data.csv"
         print(f"Writing {filename}")
@@ -101,13 +109,20 @@ def main():
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Scraper aroma-zone.')
-    parser.add_argument('--debug', help="Display debugging information", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO)
+    parser = argparse.ArgumentParser(description="Scraper aroma-zone.")
+    parser.add_argument(
+        "--debug",
+        help="Display debugging information",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.INFO,
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=args.loglevel)
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
